950e45f551331ec3d930faa6dc41cc75605f1e5d
mking94 <dadimaher94@gmail.com>
2021-10-26 14:13:07 -0700

    source code

--- readfile.c
+++ readfile.c
@@ -12,13 +12,14 @@ char line[500];
 int strtoint;
 while (fgets(line, sizeof(line), file)) {
 strtoint = atoi(line);
-for(i=2; i< strtoint/2; i++)
+for(int i=2; i< strtoint/2; i++)
 {
 if(strtoint % i == 0)
 {
 char buffer[50];
 sprintf(buffer, "%d=%d*%d", strtoint, i, strtoint/i);
 puts(buffer);
+break;
 }
 }
 }
